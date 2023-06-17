fd7e99d0c6802b0590dac9c03050e750db982c45
vishant <vishant.96.sehrawat@gmail.com>
2023-06-18 02:40:49 +0530

    admin page products created with bootstrap tabs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -58,7 +58,7 @@ userRouter.post("/login", async (req, res) => {
                     //using local storage npm package // ! not working
                     // store.set('username', { name:myUser?.name })
                     redisClient.set("refreshtoken", refreshToken)
-                    store.set("barberUser",myUser);
+                    store.set("barberUser",myUser,token,refreshToken);
                     res.status(200).send({ msg: "User logged in", token, refreshToken, usernameforchat: myUser.name, userId: myUser._id })
                 });
             }
